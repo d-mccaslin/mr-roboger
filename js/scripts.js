5d3d10ec781825f3dabf552ad436bc11f1952c18
David McCaslin <davidmccaslin94@gmail.com>
2020-08-21 11:11:34 -0700

    update business logic functions to support first couple test cases

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,38 @@ $(document).ready(function() {
   $("form#formMain").submit(function(event) {
     event.preventDefault();
     const number = parseInt($("input#numberInput").val());
-    console.log(number);
+    // console.log(number);
+    const numberRange = returnRangeOfNumbers(number);
+    const outputRange = replaceOne(numberRange);
+    console.log(outputRange);
   })
 })
 
 // Business logic
 
 function returnRangeOfNumbers(number) {
+  let numberArray = [];
+  for (let i = 0; i <= number; i++) {
+    numberArray.push(i);
+  }
+  return numberArray;
+}
+
+function replaceOne(numberArray) {
+  for (let i = 0; i < numberArray.length; i++) {
+    let targetNumber = numberArray[i];
+    if (targetNumber.toString().indexOf('1') > -1) {
+      numberArray[i] = "Beep!";
+    }
+  }
+  return numberArray;
+}
+
+function replaceTwo(numberArray) {
   return number;
 }
 
-function replaceOne(number) {
-  return number;
-}
-
-function replaceTwo(number) {
-  return number;
-}
-
-function replaceThree(number) {
+function replaceThree(numberArray) {
   return number;
 }
 
